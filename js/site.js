8368f6b1d4a1f61a3cc1f42751e648fd464cae53
diwashrestha <diwash712@gmail.com>
2020-09-07 08:48:02 +0545

    Created new piano app'

--- js/site.js
+++ js/site.js
@@ -1,89 +1,70 @@
 
-document.addEventListener("keypress", event =>{
-    let pianoKeys = ["KeyA",
-        "KeyS",
-        "KeyD", "KeyF", "KeyG","KeyH","KeyJ"];
- 
+const pianoKeys = { Backquote:"C3",KeyQ:"D3",KeyW:"E3",KeyE:"F3",KeyR:"G3",KeyT:"A3",KeyY:"B3",KeyU:"C4",KeyI:"D4",KeyO:"D4",KeyP:"F4",
+    KeyZ:"G4",KeyX:"A4",KeyC:"B4",KeyV:"C5",KeyB:"D5",KeyN:"E5",KeyM:"F5",Comma:"G5",Period:"A5",Slash:"B5",Digit1:"C_3",Digit2:"D_3",
+    Digit4:"F_3",Digit5:"G_3",Digit6:"A_3",Digit8:"C_4",Digit9:"D_4",KeyA:"F_4",KeyS:"G_4",KeyD:"A_4",KeyG:"C_5",KeyH:"D_5",KeyK:"F_5",KeyL:"G_5",Semicolon:"A_5"};
 
-    if(event.code.includes("KeyA"))
-    {
-        let audio = new Audio("music/A.mp3");
-        console.log("A");
-        audio.play();
+const pianoNote = Object.values(pianoKeys);
+
+let audioObjs = {};
+pianoNote.forEach(loadAudio);
+//pianoKeys.forEach(loadAudio)
+
+document.addEventListener("keydown", event =>{
+    const letter = event.code
+    if(pianoKeys.hasOwnProperty(letter)){
+        loadAudio(pianoKeys[letter])
+        console.log(`The '${letter}' key is pressed.`)
+        audioObjs[pianoKeys[letter]].play()
+
+        // print the key note
+        let keynote = pianoKeys[letter].replace(/_/g, "#");
+        document.getElementById('keynote-name').innerHTML = keynote;
     }
-    else if(event.code.includes("KeyS"))
+    else
     {
-        let audio = new Audio("music/S.mp3");
-        audio.play();
-    }
-    else if(event.code.includes("KeyD"))
-    {
-        let audio = new Audio("music/D.mp3");
-        audio.play();
-    }
-    else if(event.code.includes("KeyF"))
-    {
-        let audio = new Audio("music/F.mp3");
-        audio.play();
-    }
-    else if(event.code.includes("KeyG"))
-    {
-        let audio = new Audio("music/G.mp3");
-        audio.play();
-    }
-    else if(event.code.includes("KeyH"))
-    {
-        let audio = new Audio("music/H.mp3");
-        audio.play();
-    }
-    else if(event.code.includes("KeyJ"))
-    {
-        let audio = new Audio("music/J.mp3");
-        audio.play();
-    }
-    else if(event.code.includes("KeyW"))
-    {
-        let audio = new Audio("music/W.mp3");
-        audio.play();
-    }
-    else if(event.code.includes("KeyE"))
-    {
-        let audio = new Audio("music/E.mp3");
-        audio.play();
-    }
-    else if(event.code.includes("KeyT"))
-    {
-        let audio = new Audio("music/T.mp3");
-        audio.play();
-    }
-    else if(event.code.includes("KeyY"))
-    {
-        let audio = new Audio("music/Y.mp3");
-        audio.play();
-    }
-    else if(event.code.includes("KeyU"))
-    {
-        let audio = new Audio("music/U.mp3");
-        audio.play();
-    }
-    else{
-        console.warn("Unexpected keypress: '${event.key}")
+        console.warn(`Unexpected keypress: '${event.code}'`)
     }
 });
 
-// function for highlighting the key on keyboard with key is pressed
+function loadAudio(letter){
+
+    console.log(letter);
+    audioObjs[letter] = new Audio(`./music/keynote/${letter}.wav`);
+}
+
+/* document.addEventListener("keydown", event => {
+    const letter = event.key.toUpperCase()
+    if (pianoKeys.includes(letter)) {
+        loadAudio(letter) //Audio is preloaded but load again to satisfy the test
+        console.log(`The '${event.key}' key is pressed.`)
+        audioObjs[letter].play()
+    }
+    else {
+        console.warn(`Unexpected keypress: '${event.code}'`)
+    }
+});
+
+function loadAudio(letter) {
+    audioObjs[letter] = new Audio(`../music/${letter}.mp3`)
+} */
+
+
+
+// function for highlighting the white keys on keyboard with key is pressed
 
 document.addEventListener("keypress",event=>{
-    let whiteKeys = ["KeyA","KeyS","KeyD","KeyF","KeyG","KeyH","KeyJ","KeyK","KeyL","KeyC","KeyV","KeyB","KeyN","KeyM"];
+    let whiteKeys = ["Backquote","KeyQ","KeyW","KeyE","KeyR","KeyT","KeyY","KeyU",
+        "KeyI","KeyO","KeyP","KeyZ","KeyX","KeyC","KeyV","KeyB","KeyN","KeyM","Comma","Period","Slash"];
     if(whiteKeys.includes(event.code)){
         let pressKey = document.getElementById(event.code);
-        pressKey.style.backgroundColor = "royalblue";
+        pressKey.style.backgroundColor = "lightblue";
         pressKey.style.boxShadow = "none";
     }
 });
 
 document.addEventListener("keyup", event =>{
-    let whiteKeys = ["KeyA","KeyS","KeyD","KeyF","KeyG","KeyH","KeyJ","KeyK","KeyL","KeyC","KeyV","KeyB","KeyN","KeyM"];
+    let whiteKeys = ["Backquote","KeyQ","KeyW","KeyE","KeyR","KeyT","KeyY","KeyU",
+        "KeyI","KeyO","KeyP","KeyZ","KeyX","KeyC","KeyV","KeyB","KeyN","KeyM","Comma","Period","Slash"];
 
     whiteKeys.forEach(function(key){
         let upKey = document.getElementById(key);
@@ -97,44 +78,34 @@ document.addEventListener("keyup", event =>{
 
 // function for black keys 
 document.addEventListener("keypress",event =>{
-    let pianoKeys = ["KeyQ","KeyW","KeyE", "KeyR", "KeyT","KeyY","KeyU",
-                    "KeyI","KeyI","KeyO","KeyP"];
+    let pianoKeys = ["Digit1","Digit2","Digit4", "Digit5", "Digit6","Digit8","Digit9",
+                    "KeyA","KeyS","KeyD","KeyG","KeyH","KeyK","KeyL","Semicolon"];
         if(pianoKeys.includes(event.code)){
             console.log(`The '${event.key}' key is pressed.`)
         }
         if(pianoKeys.includes(event.code)){
             let pressKey = document.getElementById(event.code);
             pressKey.style.backgroundColor = "royalblue";
-            pressKey.style.boxShadow = "none";
+            pressKey.style.height = "7.2em";
+            pressKey.style.boxShadow = "0 4px #333";
         };
 });
 
 document.addEventListener("keyup",event =>{
-    let pianoKeys = ["KeyQ","KeyW","KeyE", "KeyR", "KeyT","KeyY","KeyU",
-    "KeyI","KeyI","KeyO","KeyP"];
+    let pianoKeys = ["Digit1","Digit2","Digit4", "Digit5", "Digit6","Digit8","Digit9",
+        "KeyA","KeyS","KeyD","KeyG","KeyH","KeyK","KeyL","Semicolon"];
     
     
     pianoKeys.forEach(function(key) {
         console.log(key);
         let upKey = document.getElementById(key);
         upKey.style.backgroundColor = "black";
-        upKey.style.boxShadow = "0 5px #444";
+        upKey.style.height = "7em";
+        upKey.style.boxShadow = "0 10px #444";
         });
 });
 
 
 
-document.onkeypress = function(e) {
-    if ((e.which || e.keyCode) == 113) { //this is the number code for the letter "S"
-  let keyS = document.getElementById('first');
-  keyS.style.backgroundColor ="royalblue";
-  keyS.style.boxShadow = "none";
-}
-};
-  
-  document.onkeyup = function(e) {
-    let keyS = document.getElementById('first');
-    keyS.style.backgroundColor = "black";
-    keyS.style.boxShadow = "0 5px #444";
-};
+
   
